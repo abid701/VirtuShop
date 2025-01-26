46d8dfa3082312b290ce19919cb0dfcd25b52d7b
Abid Erfan <abid_erfan7@yahoo.com>
2025-01-26 19:17:42 +0100

    Update ProductsController.java

--- virtuShopApp/src/main/java/com/virtuShop/virtuShopApp/web/controllers/ProductsController.java
+++ virtuShopApp/src/main/java/com/virtuShop/virtuShopApp/web/controllers/ProductsController.java
@@ -1,4 +1,30 @@
 package com.virtuShop.virtuShopApp.web.controllers;
 
+import com.virtuShop.virtuShopApp.db.entities.Products;
+import com.virtuShop.virtuShopApp.services.ProductsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductsController {
+
+    private final ProductsService productsService;
+
+    public ProductsController(ProductsService productsService){
+        this.productsService = productsService;
+    }
+
+
+    @GetMapping("/homePage")
+    public String showRandomProductsList(Model model){
+
+        Iterable<Products> productsList;
+
+        productsList = productsService.getAllProducts();
+        model.addAttribute("productsList", productsList);
+
+        return "view/homePage";
+    }
 }
+
